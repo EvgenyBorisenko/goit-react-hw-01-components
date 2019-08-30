33a25b16a79476b8fde51f4a4ecddae0148b62ca
EvgenyBorisenko <borisenkoevgeny1991@gmail.com>
2019-08-30 15:57:09 +0300

    Updates

---
+++ precache-manifest.013e55aea41b40499f307c54e49d16df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de6f4433f456716fc7a8ab779c74de10",
+    "url": "/test_repa1/index.html"
+  },
+  {
+    "revision": "9d1ea6f07531da945afb",
+    "url": "/test_repa1/static/css/main.3f38ea96.chunk.css"
+  },
+  {
+    "revision": "e76183626b321421475a",
+    "url": "/test_repa1/static/js/2.9fbc1c49.chunk.js"
+  },
+  {
+    "revision": "9d1ea6f07531da945afb",
+    "url": "/test_repa1/static/js/main.278cd9fb.chunk.js"
+  },
+  {
+    "revision": "8e0283fd855ee0f85a64",
+    "url": "/test_repa1/static/js/runtime~main.3e5fb7a6.js"
+  }
+]);
